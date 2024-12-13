d36dbe3d76e0e885960fe36495db579859600273
vitalii.soboliev <vitalii.soboliev@sigma.software>
2024-12-13 15:06:01 +0100

    Added home/housing components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import {Component} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [HomeComponent, RouterModule],
   template: `
-    <h1>Default</h1>
+    <main>
+      <a href="/">
+      <!--<a [routerLink]="['/']">-->
+        <header class="brand-name">
+          <p>HOME</p>
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
   `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'default';
+  title = 'homes';
 }
---
+++ src/app/routes.ts
@@ -0,0 +1,15 @@
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+const routeConfig: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home page',
+  },
+/*  {
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Home details',
+  },*/
+];
+export default routeConfig;
